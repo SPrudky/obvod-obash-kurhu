f63300192b54d2fbf67cbe69ae6be29c14d0d358
Samuel Prudký <Prudky.Samuel@stud.vassboskovice.cz>
2021-11-02 11:23:35 +0100

    hotovo

--- obvod obash kurhu/Program.cs
+++ obvod obash kurhu/Program.cs
@@ -6,11 +6,45 @@ namespace obvod_obash_kurhu
     {
         static void Main(string[] args)
         {
+            double polomer;
+            double prumer;
+            double vyber;
+            double obvod;
+            double obsah;
+
             Console.WriteLine("KRUH");
 
+            Console.WriteLine("Vyberte z možností (1 polomer)" +
+                "                                 (2 prumer)");
+            double.TryParse(Console.ReadLine(), out vyber);
 
 
+            
+            
 
+
+            if (vyber == 1)
+            {
+                Console.WriteLine("Zadejte polomer");
+                Double.TryParse(Console.ReadLine(), out polomer);
+                obvod = 2 * 3.14 * polomer;
+                obsah = 3.14 * (polomer * polomer);
+                Console.WriteLine($"o = {obvod}" +
+                    $" S = {obsah}");
+                
+            }
+            else if (vyber == 2)
+            {
+                Console.WriteLine("Zadejte prumer");
+                Double.TryParse(Console.ReadLine(), out prumer);
+                obvod = 3.14 * prumer;
+                obsah = 3.14 * (prumer * prumer) / 4;
+                Console.WriteLine($"o = {obvod}" +
+                    $" S = {obsah}");
+
+            }
+              Console.ReadLine();
+            Math.Round( 0.1);
 
 
 
